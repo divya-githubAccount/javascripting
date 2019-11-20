a41efdc690fc32314cff41d450224f218caa7540
divya-githubAccount <divyaumb99@gmail.com>
2019-11-20 15:27:48 +0530

    this is javascript commit

---
+++ if-statement.js
@@ -0,0 +1,6 @@
+var fruit='orange.'
+var s=fruit.length
+if(s<=5)
+console.log('The fruit name has five characters or less.')
+else
+console.log('The fruit name has more than five characters.')
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+var pizza='pizza is alright'
+var x=pizza.replace('alright','wonderful')
+console.log(x)
